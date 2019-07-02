eb5405bf23da4a0060fdc9173efa3ecfb703d896
Ella Churina <ellachur@gmail.com>
2019-07-02 22:12:05 +0300

    Добавила домашнее задание ко 2-му уроку

--- VK_App/VK_App/LoginController.swift
+++ VK_App/VK_App/LoginController.swift
@@ -14,11 +14,25 @@ class LoginController: UIViewController {
     
     @IBOutlet var passwordTextField: UITextField!
     
+    @IBOutlet var scrollView: UIScrollView!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        NotificationCenter.default.addObserver(self, selector:
+            #selector(keyboardWasShown(notification:)), name:
+            UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector:
+            #selector(keyboardWasHidden(notification:)), name:
+            UIResponder.keyboardWillHideNotification, object: nil)
+        
+        navigationController?.navigationBar.isHidden = true
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -27,15 +41,56 @@ class LoginController: UIViewController {
 
     override func viewDidDisappear(_ animated: Bool) {
         super.viewDidDisappear(animated)
+        
+        NotificationCenter.default.removeObserver(self)
+        
+        navigationController?.navigationBar.isHidden = false 
     }
     
     override func viewWillLayoutSubviews() {
         super.viewWillLayoutSubviews()
     }
 
-    @IBAction func logInButtonPress(_ sender: Any) {
-        print("username: \(userNameTextField.text)")
-        print("username: \(passwordTextField.text)")
+   @IBAction func logInButtonPress(_ sender: Any) {
+        print("username: \(userNameTextField.text ?? "")")
+        print("username: \(passwordTextField.text ?? "")")
+        
+        performSegue(withIdentifier: "LoginSegue", sender: nil)
+        
     }
+    
+    @objc private func keyboardWasShown(notification: Notification) {
+        let info = notification.userInfo as NSDictionary?
+        let keyboardSize = (info?.value(forKey: UIResponder.keyboardFrameEndUserInfoKey) as!
+        NSValue).cgRectValue.size
+        let contentInsent = UIEdgeInsets(top: 0, left: 0, bottom: keyboardSize.height, right: 0)
+        scrollView.contentInset = contentInsent
+        scrollView.scrollIndicatorInsets = contentInsent
+    }
+    
+    @objc private func keyboardWasHidden(notification: Notification) {
+        let contentInsent = UIEdgeInsets.zero
+        scrollView.contentInset = contentInsent
+        scrollView.scrollIndicatorInsets = contentInsent
+    }
+    
+    override func performSegue(withIdentifier identifier: String, sender: Any?) {
+        if shouldPerformSegue(withIdentifier: identifier, sender: sender) {
+            super.performSegue(withIdentifier: identifier, sender: sender)
+        }
+    }
+    
+    override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
+        if identifier == "LoginSegue",
+            userNameTextField.text == "admin",
+            passwordTextField.text == "123456" {
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    
+    
 }
 
