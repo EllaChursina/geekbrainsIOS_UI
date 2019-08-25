2e05fef8ab9fe8b935da13cf906f707ef93e92b3
Ella Churina <ellachur@gmail.com>
2019-08-25 19:28:25 +0300

    добавлен like control

---
+++ VK_App/VK_App/Custom/LikeControl.swift
@@ -0,0 +1,21 @@
+//
+//  LikeControl.swift
+//  VK_App
+//
+//  Created by MacBook on 21/08/2019.
+//  Copyright © 2019 EllaChursina. All rights reserved.
+//
+
+import UIKit
+
+class LikeControl: UIControl {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
