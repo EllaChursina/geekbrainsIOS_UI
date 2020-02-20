4e7782425bf2c47067c5dd7e3caeaa94e896ad8d
Элла Чурсина <ellachursina@MacBook-Pro-Ella.local>
2020-02-20 19:20:35 +0300

    Добавлено домашнее задание к 1-му уроку по клиент-серверной части

---
+++ VK_App/VK_App/Controllers/DetailFriendsPhotoController.swift
@@ -0,0 +1,21 @@
+//
+//  DetailFriendsPhotoController.swift
+//  VK_App
+//
+//  Created by Элла Чурсина on 06.02.2020.
+//  Copyright © 2020 EllaChursina. All rights reserved.
+//
+
+import UIKit
+
+class DetailFriendsPhotoController: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ VK_App/VK_App/Singleton/Session.swift
@@ -0,0 +1,9 @@
+//
+//  Session.swift
+//  VK_App
+//
+//  Created by Элла Чурсина on 20.02.2020.
+//  Copyright © 2020 EllaChursina. All rights reserved.
+//
+
+import Foundation
